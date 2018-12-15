d29fbcf6a8cff475d689b5186949c2c019fadc41
Leandro Abraham <leandromaf@gmail.com>
2018-12-15 11:57:59 -0300

    commit inicial de expertos en Base de datos y en deteccion de mutantes

---
+++ DataBaseExperts.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pdb
+
+# Experto para manejo de base de datos MongoDB
+class MongoDataBaseExpert:
+
+	# funcion que agrega un dna a la base (si no existe) especificando si es mutante o no
+	def saveDna(self,dna):
+		# TODO implement it 
+		pass
+
+	# funcion que  devuelve las estadísticas de las verificaciones de ADN cuantos mutnates y no mutantes hay y su ratio
+	def getStats(self):
+		# TODO implement
+		pass
+
+
---
+++ MutantDetectionExperts.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import pdb
+
+
+# Eperto en deteccion de mutantes analizando su DNA
+class DNAMutantDetectionExpert:
+
+	# funcion que debe decir si el arreglo de cadenas pasado en dna corresponde a un mutante
+	def isMutant(self,dna):
+		# TODO implement
+		# para decidir debe verificar si hay  más de una secuencia de cuatro letras iguales​, de forma oblicua, horizontal o vertical
+		pass
